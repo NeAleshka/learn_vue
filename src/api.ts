774ce5edd2ac5260346e2ba297d0c2971feeb509
Алексей <alexeybudai@gmail.com>
2023-04-26 20:44:59 +0300

    pre-websocket

--- src/api.ts
+++ src/api.ts
@@ -1,25 +1,5 @@
 import axios from "axios";
 
-/* const api = {
-  loadTicker: async (tickersName: string[]) => {
-    const data = await fetch(
-      `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${tickersName.join(
-        ","
-      )}`
-    );
-    return await data.json();
-  },
-  getCoinsList: async () => {
-    const data = await fetch(
-      "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
-    );
-    const res = await data.json();
-    return res.Data;
-  },
-};
-
-export default api; */
-
 const instance = axios.create({
   baseURL: "https://min-api.cryptocompare.com/data/",
 });
